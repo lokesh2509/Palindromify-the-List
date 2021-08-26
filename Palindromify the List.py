1850de90daf90e80fdd60bb8dddf6051fff4ca87
Lokesh Vyas <79556675+lokesh1814@users.noreply.github.com>
2021-08-26 13:42:30 +0530

    Update Palindromify the List.py

--- Palindromify the List.py
+++ Palindromify the List.py
@@ -1,19 +1,3 @@
-"""
-The task you have to perform is “Palindromify the List.” This task consists of a total of 10 points to evaluate your performance.
-
-Problem Statement:-
-You are given a list that contains some numbers. You have to print a list of next palindromes only if the number is
-greater than 10; otherwise, you will print that number.
-
-
-
-Input:
-[1, 6, 87, 43]
-
-Output:
-[1, 6, 88, 44]
-"""
-
 lst = input("Enter the list of numbers:- "). split( )
 for i in range(len(lst)):
     while True:
